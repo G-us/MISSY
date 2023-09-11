3bf63aa9907d0542c8e2d1127a0edbbb28d81f7f
G_us <henrygparsons@gmail.com>
2023-09-11 17:44:26 +0200

    Removing spotify dependency

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+
 import apscheduler
 import asyncio
 import websockets
@@ -8,24 +7,22 @@ sad = "spotify:track:6xCCgnYGGDWVXqfHzdMkCQ"
 jazz = "spotify:track:2ifRmHoyg1f2pUtFhlqrn2"
 waiting = "spotify:track:1ZlnsAT3J7vmf0xuOMHD9V"
 
-scope = "user-library-read, streaming, user-read-playback-state, user-modify-playback-state, user-read-currently-playing"
-print("Opening Spotify...")
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
 
 state = 0
 
 
-def useHeartData(heartRate, SpotifyPlay):
+def useHeartData(heartRate):
     global state
     if heartRate > 90 and state != 1:
         print("Heart rate is too high!")
-        playSong("spotify:track:7cO24uuqaMv6YPImQinBgQ", fade=False, sp=SpotifyPlay)
+        playSong("spotify:track:7cO24uuqaMv6YPImQinBgQ", fade=False)
         print(state)
         state = 1
         print(state)
     elif heartRate < 60 and state != 2:
         print("Heart rate is too low!")
-        playSong("spotify:track:3qs1ozCx271UQqmzC7oNuj", fade=False, sp=SpotifyPlay)
+        playSong("spotify:track:3qs1ozCx271UQqmzC7oNuj", fade=False)
         print(state)
         state = 2
         print(state)
@@ -43,7 +40,7 @@ async def receive_data():
                     print("Received:", data)
                     if "heartRate" in data:
                         heartRate = data.split(":")[1]
-                        useHeartData(int(heartRate), sp)
+                        useHeartData(int(heartRate))
         except websockets.exceptions.ConnectionClosed:
             print("Connection was closed. Reconnecting...")
         except Exception as e:
@@ -51,14 +48,13 @@ async def receive_data():
         await asyncio.sleep(5)  # Wait for a few seconds before reconnecting
 
 
-def playSong(uri, fade, sp):
+def playSong(uri, fade):
     if fade:
-        startingVolume = sp.current_playback()["device"]["volume_percent"]
-        for i in range(startingVolume, 0):
+
+        for i in range(100, 0):
             print(i)
-            print("Raising volume")
-            sp.volume(i)
-        sp.start_playback(uris=[uri])
+            print("Raising volume: " + str(i))
+        print("Playing song: " + uri)
 
 
 if __name__ == "__main__":
